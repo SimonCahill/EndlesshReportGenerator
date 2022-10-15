2505f722a0263a87a2736fd8dbd9692da73cd6b9
Simon Cahill <simon@h3lix.de>
2022-10-15 22:14:28 +0200

    Removed all extension functions from main TU

--- src/main.cpp
+++ src/main.cpp
@@ -71,16 +71,9 @@ struct ConnectionDetails {
     usedPorts({}), totalSecondsWasted(0), totalBytesSent(0), host({}) {}
 };
 
-bool                                    splitString(const string& str, const string& delimiters, vector<string> &tokens); //!< Splits a string by one or more delimiters
-bool                                    regexMatch(const char* haystack, const char* needle); //!< Matches a string against a regular expression
-double  	                            roundNumber(const double x, const uint32_t decimalPlaces); //!<
 int32_t                                 parseArgs(const int32_t&, char**); //!< Parses command-line arguments
 map<string, pair<uint32_t, uint32_t>>   getConnections(const vector<string>&); //!< Gets the logged connections
 string                                  getCurrentIsoTimestamp(); //!< Gets the current time as an ISO timestamp, accurate to the current second.
-string                                  getSpacerString(const uint32_t totalWidth, const uint32_t strLength); //!< Gets the spacer string (might be removed)
-string                                  trimStart(string nonTrimmed, const string& trimChar); //!< Trims the start of a string
-string                                  trimEnd(string nonTrimmed, const string& trimChar); //!< Trims the end of a string
-string                                  trim(const string& nonTrimmed, const string& trimChar); //!< Trims a string
 vector<ConnectionDetails>               getDetailledConnections(const vector<string>&); //!< Gets a detailled list of logged connections
 vector<string>                          readEndlesshLog(); //!< Reads the log file into memory
 void                                    printConnectionStatistics(const uint32_t uniqueIps, const uint32_t totalAccepted, const uint32_t totalClosed, const double totalTimeWasted, const uint32_t totalBytesSent); //!< Print connection statistics
@@ -512,140 +505,6 @@ void printDetailedIpStats(const vector<ConnectionDetails>& connectionList, uint3
         cout << endl;
     }
 }
-
-/**
- * @brief Splits a given string by the passed delimiters in to a vector.
- *
- * @param str string& The string to split.
- * @param delimiters string& A string containing the delimiters to split by (chars).
- * @param tokens vector<string>& A vector that will contain the elements.
- * 
- * @return true If tokens were found.
- * @return false Otherwise.
- */
-bool splitString(const string& str, const string& delimiters, vector<string> &tokens) {
-    // Skip delimiters at beginning.
-    string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-    // Find first "non-delimiter".
-    string::size_type pos = str.find_first_of(delimiters, lastPos);
-
-    while (string::npos != pos || string::npos != lastPos) {
-        // Found a token, add it to the vector.
-        tokens.push_back(str.substr(lastPos, pos - lastPos));
-        // Skip delimiters.
-        lastPos = string::npos == pos ? string::npos : pos + 1;
-        // Find next "non-delimiter"
-        pos = str.find_first_of(delimiters, lastPos);
-    }
-
-    return tokens.size() > 0;
-}
-
-/**
- * @brief Matches a string against a regular expression.
- * 
- * @param haystack The string to compare
- * @param needle The expression to compare against
- * 
- * @return true If the string matches the expression
- * @return false Otherwise
- */
-bool regexMatch(const char* haystack, const char* needle) {
-    int nRet;
-    regex_t sRegEx;
-
-    if (regcomp(&sRegEx, needle, REG_EXTENDED | REG_NOSUB) != 0) {
-        return false;
-    }
-
-    nRet = regexec(&sRegEx, haystack, (size_t) 0, NULL, 0);
-    regfree(&sRegEx);
-
-    if (nRet != 0) {
-        return false;
-    }
-
-    return true;
-}
-
-/**
- * @brief Rounds a given double to the desired amount of decimal places.
- * 
- * @param x The double to round
- * @param decimalPlaces The decimal places the double should be rounded to
- * 
- * @return double The resulting double.
- */
-double roundNumber(const double x, const uint32_t decimalPlaces) {
-    const double multiplificationFaktor = std::pow(10.0, decimalPlaces);
-    return std::ceil(x * multiplificationFaktor) / multiplificationFaktor;
-}
-
-/**
- * @brief Gets the current timestamp as an ISO timestamp, accurate to the nearest second.
- * 
- * @return string A string containing the current timestamp in ISO format.
- */
-string getCurrentIsoTimestamp() {
-    auto timeNow = system_clock::now();
-    return date::format("%FT%TZ", date::floor<seconds>(timeNow));
-}
-
-/**
- * @brief Gets a string containing whitespace to centre text
- * 
- * @param totalWidth The total width of the area where the string should be centred
- * @param strLength The length of the string to be centre-printed
- * 
- * @return string The spacer string
- */
-string getSpacerString(const uint32_t totalWidth, const uint32_t strLength) {
-    return string(totalWidth / 2 - strLength / 2, ' ');
-}
-
-/**
- * @brief Trims the beginning of a given string.
- *
- * @param nonTrimmed The non-trimmed string.
- * @param trimChar The character to trim off the string. (default: space)
- *
- * @return The trimmed string.
- */
-string trimStart(string nonTrimmed, const string& trimChar) {
-    //nonTrimmed.erase(nonTrimmed.begin(), find_if(nonTrimmed.begin(), nonTrimmed.end(), not1(ptr_fun<int32_t, int32_t>(isspace))));
-
-    function<bool(char)> shouldTrimChar = [=](char c) -> bool { return trimChar.size() == 0 ? isspace(c) : trimChar.find(c) != string::npos; };
-
-    nonTrimmed.erase(nonTrimmed.begin(), find_if(nonTrimmed.begin(), nonTrimmed.end(), not1(shouldTrimChar)));
-
-    return nonTrimmed;
-}
-
-/**
- * @brief Trims the end of a given string.
- * @param nonTrimmed The non-trimmed string.
- * @param trimChar The character to trim off the string. (default: space)
- *
- * @return The trimmed string.
- */
-string trimEnd(string nonTrimmed, const string& trimChar) {
-    // nonTrimmed.erase(find_if(nonTrimmed.rbegin(), nonTrimmed.rend(), not1(ptr_fun<int32_t, int32_t>(isspace))).base(), nonTrimmed.end());
-
-    function<bool(char)> shouldTrimChar = [=](char c) -> bool { return trimChar.size() == 0 ? isspace(c) : trimChar.find(c) != string::npos; };
-    nonTrimmed.erase(find_if(nonTrimmed.rbegin(), nonTrimmed.rend(), not1(shouldTrimChar)).base(), nonTrimmed.end());
-
-    return nonTrimmed;
-}
-
-/**
- * @brief Trims both the beginning and the end of a given string.
- *
- * @param nonTrimmed The non-trimmed string.
- * @param trimChar The character to trim off the string. (default: space)
- *
- * @return The trimmed string.
- */
-string trim(const string& nonTrimmed, const string& trimChar) { return trimStart(trimEnd(nonTrimmed, trimChar), trimChar); }
 
 /**
  * @brief Parses arguments passed to the application and sets values accordingly.
