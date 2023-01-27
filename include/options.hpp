eef2e4c7d6e1bad44e28890c8c799af68295c75c
Simon Cahill <simon@simonc.eu>
2023-01-27 20:49:31 +0100

    Added missing comments, fixed spacing in help text.

--- include/options.hpp
+++ include/options.hpp
@@ -25,8 +25,18 @@
 using std::string;
 using std::string_view;
 
+/**
+ * @brief Gets the string containing the application's short arg string.
+ * 
+ * @return constexpr string_view The arg string as required for @see getopt_long
+ */
 constexpr string_view   getAppArgs() { return R"(icsandhvS:)"; }
 
+/**
+ * @brief Gets the application's command-line options for @see getopt_long.
+ * 
+ * @return const option* A const pointer to the array containing the options.
+ */
 const option*           getAppOptions() {
     const static option OPTIONS[] = {
         { "no-ip-stats",    no_argument,        nullptr,    'i' },
@@ -43,6 +53,13 @@ const option*           getAppOptions() {
     return OPTIONS;
 }
 
+/**
+ * @brief Gets the application's help text.
+ * 
+ * @param binName The name of the binary.
+ * 
+ * @return string The actual help text.
+ */
 inline string           getAppHelpText(const string_view& binName = getProjectName()) {
     const static string HELP_TEXT_FMT = R"(
 {0:s} v{1:s} - {2:s}
@@ -64,12 +81,17 @@ Switches:
     --version,      -v      Display version information and exit
 
 Arguments:
-    --syslog [f],   -S[f]      Override syslog/endlessh log location
+    --syslog [f],   -S[f]   Override syslog/endlessh log location
 )";
 
     return fmt::format(HELP_TEXT_FMT, binName, getApplicationVersion(), getProjectDescription());
 }
 
+/**
+ * @brief Gets the application's version information.
+ * 
+ * @return string The string containing the version information.
+ */
 inline string           getAppVersionText() { return fmt::format(R"({0:s} v{1:s} - {2:s})", getProjectName(), getApplicationVersion(), getProjectDescription()); }
 
 #endif // ENDLESSH_REPORT_INCLUDE_OPTIONS_HPP
